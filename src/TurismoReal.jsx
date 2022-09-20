472be54c6009dc5d7abcde4d3968b38b80c15664
Lampiosko <claudio.abele@gmail.com>
2022-09-20 13:11:20 -0300

    Header

---
+++ src/TurismoReal.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Header } from './components/Header';
+import { AppRouter } from './router/AppRouter';
+
+export const TurismoReal = () => {
+    return (
+    <>
+       
+       <AppRouter></AppRouter>
+    </>
+    )
+}
---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,25 @@
+import { Routes, Route, BrowserRouter as Router, Navigate } from "react-router-dom";
+import { Departamentos } from "../pages/Departamentos";
+import { Tours } from "../pages/Tours";
+import { Header } from "../components/Header";
+import { Contactanos } from "../pages/Contactanos";
+import { Nosotros } from "../pages/Nosotros"
+
+export const AppRouter = () => {
+    return (
+        <> 
+        <Router>
+        <Header/>   
+            <Routes>
+                <Route path='departamentos' element={<Departamentos/>} />
+                <Route path='tours' element={<Tours/>} />
+                <Route path='nosotros' element={<Nosotros/>} />
+                <Route path='contactanos' element={<Contactanos/>} />
+                <Route path='*' element={<Navigate to='/departamentos'/>} /> 
+            </Routes>
+        </Router>
+            
+        </>
+    )
+}
+
