c72ebdddfae1988bf737e6ee8b071eb68229d817
Diego Quezada <71034913+dQuezada-P@users.noreply.github.com>
2022-10-01 13:56:27 -0300

    update

--- src/components/department/DepartmentCard.jsx
+++ src/components/department/DepartmentCard.jsx
@@ -4,27 +4,28 @@ import { MdBathtub,IoBedSharp } from "react-icons/all";
 
 export const DepartmentCard = ({ depto }) => {
   const {
-    id,
-    nombre,
-    numero_banno,
-    numero_habitacion,
-    fecha_ins,
-    direccion,
-    valor_arriendo,
-    ubicacion,
-    descripcion,
+    ID,
+    NOMBRE,
+    NUMERO_BANNO,
+    NUMERO_HABITACION,
+    FECHA_INS,
+    DIRECCION,
+    VALOR_ARRIENDO,
+    UBICACION,
+    DESCRIPCION,
+    IMAGENES
   } = depto;
   
 
   return (
     <>
-      <NavLink className='' to={`/departamento/${{id}}`}><img src="" alt="imagen" /></NavLink>
-      <h3 className='font-semibold text-2xl ml-3 py-2'>${valor_arriendo} CLP</h3>
-      <h4 className='font-semibold ml-3 py-2'>{ubicacion}</h4>
+      <NavLink className='' to={`/departamento/${{ID}}`}><img src="" alt="imagen" /></NavLink>
+      <h3 className='font-semibold text-2xl ml-3 py-2'>${VALOR_ARRIENDO} CLP</h3>
+      <h4 className='font-semibold ml-3 py-2'>{UBICACION}</h4>
       <div className='flex justify-around py-2 '>
         <div>
         <h4 className="flex gap-2 items-center text-2xl font-semibold ">
-          {numero_habitacion}
+          {NUMERO_HABITACION}
           <span>
             <IoBedSharp />
           </span>
@@ -32,7 +33,7 @@ export const DepartmentCard = ({ depto }) => {
         </div> 
         <div>
         <h4 className="flex gap-2 items-center text-2xl font-semibold">
-          {numero_banno}
+          {NUMERO_BANNO}
           <span>
             {" "}
             <MdBathtub />
@@ -41,7 +42,7 @@ export const DepartmentCard = ({ depto }) => {
         </div>
         <div>
         <h4 className="flex text-lg font-semibold">
-          {nombre}
+          {NOMBRE}
           <span></span>
         </h4>
         </div>
