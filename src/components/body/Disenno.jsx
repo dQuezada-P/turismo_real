37630eb271b76674519d626e7a94d1c83191ba3d
Lampiosko <claudio.abele@gmail.com>
2022-10-12 15:55:25 -0300

    component design

--- src/components/body/Disenno.jsx
+++ src/components/body/Disenno.jsx
@@ -1,4 +1,11 @@
-export default function Disenno({children}) {
+export default function Disenno({
+  children,
+  title,
+  desc1,
+  name,
+  desc2,
+  desc3,
+}) {
   return (
     <>
       {" "}
@@ -7,22 +14,23 @@ export default function Disenno({children}) {
           <div className=" bg-yellow-400 bg-overlap-black"></div>
           <div className=" bg-white bg-overlap-black"></div>
         </div>
-        <div className="container-body z-20 relative text-white mb-28">
-          <h1 className="text-2xl font-bold mb-8">
-            Servicios Tour en zonas turísticas
+        <div className="p-8  mx-6 md:mx-40 box-content container z-20 relative text-white md:mb-28 ">
+          <h1 className="text-3xl font-bold mx-8 md:mx-0 py-8 md:py-0 mb-4 capitalize ">
+            {title}
           </h1>
-          <p className="text-justify">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Perferendis, aliquid? Laboriosam maiores tenetur{" "}
-            <span className="text-yellow-400">vitae quidem ipsum</span> unde
-            commodi nemo alias ipsam nam, incidunt accusantium in adipisci eius,
-            eligendi, perspiciatis corrupti at sequi molestiae praesentium. Ut
-            totam harum placeat, velit veritatis accusantium, doloribus
-            asperiores repudiandae magnam maxime impedit nemo assumenda a.
+          <p className="text-justify mb-4 invisible md:visible">
+            {desc1} <span className="text-yellow-400">{name}</span> {desc2}
           </p>
+          <p className="text-justify invisible md:visible">{desc3}</p>
         </div>
       </div>
-      {children}
+      <div className="h-full w-full bg-white relative z-20 overflow-hidden">
+        <div className="bg-bars-body absolute">
+          <div className=" bg-yellow-400 "></div>
+          <div className=" bg-purple-600"></div>
+        </div>
+        {children}
+      </div>
     </>
   );
 }
--- src/pages/Departamentos.jsx
+++ src/pages/Departamentos.jsx
@@ -16,35 +16,40 @@ export const Departamentos = () => {
     </div>
   ) : (
     <>
-      <Body><div></div></Body> 
-      <div
-        className={`${
-          departments.length < 5
-            ? " mx-6 md:mx-40 md:h-[calc(100vh-176px)]  bg-white relative z-20 overflow-hidden"
-            : " mx-6 md:mx-40 bg-white relative z-20 overflow-hidden"
-        }`}
+      <Body
+        title={"Arriendo de departamentos"}
+        name={"Turismo Real"}
+        desc1={"Si deseas viajar y conocer lugares turísticos en el país,"}
+        desc2={
+          "es el lugar correcto. Como empresa ofrecemos los mejores servicios de arriendo de departamentos, servicio de transporte y también tours a las mejores zonas turísticas cercanas a los departamentos."
+        }
+        desc3={'Para la comodidad de nuestros clientes ofrecemos la facilidad de interactuar con nosotros de manera online, fácil y secilla, si presentas cualquier consulta no dudes en hablarnos, toda la información se encuentra en la sección'}
       >
-        <div className="bg-bars-body absolute">
-          <div className=" bg-yellow-400 "></div>
-          <div className=" bg-purple-600"></div>
+        <div
+          className={`${
+            departments.length < 5
+              ? " mx-6 md:mx-40 md:h-[calc(100vh-176px)]"
+              : " mx-6 md:mx-40 "
+          }`}
+        >
+          <DepartmentFilter />
+          {departments == [] ? (
+            <div className="w-full h-full text-center md:flex md:items-start md:pt-20 md:justify-center relative z-30 ">
+              <p className="text-4xl font-semibold text-purple-700">
+                No Hay Departamentos Disponibles
+              </p>
+            </div>
+          ) : (
+            <div className="grid md:grid-cols-4 mb-12 gap-4 relative z-30 ">
+              {departments.map((depto) => (
+                <div className="shadow-2xl rounded-3xl bg-white" key={depto.ID}>
+                  <DepartmentCard depto={depto} />
+                </div>
+              ))}
+            </div>
+          )}
         </div>
-        <DepartmentFilter />
-        {departments == [] ? (
-          <div className="w-full h-full text-center md:flex md:items-start md:pt-20 md:justify-center relative z-30">
-            <p className="text-4xl font-semibold text-purple-700">
-              No Hay Departamentos Disponibles
-            </p>
-          </div>
-        ) : (
-          <div className="grid md:grid-cols-4 mb-12 gap-4 relative z-30 ">
-            {departments.map((depto) => (
-              <div className="shadow-2xl rounded-3xl bg-white" key={depto.ID}>
-                <DepartmentCard depto={depto} />
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
+      </Body>
     </>
   );
 };
