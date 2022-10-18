ece4c86a5488b9aa9d7f11ec0bd430f918be0ea9
Rafael Mendez <raf.mendez@duocuc.cl>
2022-10-18 20:50:09 -0300

    a(sufrimiento)

--- src/pages/Reserva.jsx
+++ src/pages/Reserva.jsx
@@ -1,9 +1,34 @@
 import { useEffect } from "react";
 import DesignSecondary from "../components/body/DesignSecondary";
 import Login from "./Login";
+import {useDepartment} from "../context/hooks/useDepartment";
+import { IoBedOutline } from "react-icons/io5";
+import { TbBath } from "react-icons/tb";
+import { MdOutlineChair } from "react-icons/md";
+import { BsBuilding } from "react-icons/bs";
+import { BiMap } from "react-icons/bi";
 
 export const Reserva = () => {
-  
+
+  const {department,images} = useDepartment()
+  const {ID,
+    NOMBRE,
+    NUMERO_BANNO,
+    NUMERO_HABITACION,
+    FECHA_INS,
+    DIRECCION,
+    VALOR_ARRIENDO,
+    UBICACION,
+    DESCRIPCION,
+    IMAGENES,} = department
+
+  const newValorArriendo = Intl.NumberFormat("es-CL", {
+    currency: "CLP",
+    style: "currency",
+  }).format(VALOR_ARRIENDO);
+
+  console.log(department)
+
   return (
     <>
       <DesignSecondary>
@@ -13,8 +38,66 @@ export const Reserva = () => {
             <div className="BoxFlex flex flex-col md:flex-row w-full h-[90%] pt-4 px-4">
               <div className="BoxUser basis-[60%] h-full">
                 {/* <Login></Login> */}
+                {/* Recuadro Izquierdo*/}
+                <div className="flex-auto w-1 h-1 bg-black">
+                  {
+                    
+                  }
+                </div>
               </div>
-              <div className="BoxResumary basis-[40%] h-full border-t-[1px] md:border-l-[1px] md:border-t-[0px]  border-slate-300"></div>
+
+              {/* Recuadro Derecho*/}
+              <div className="BoxResumary basis-[40%] h-full border-t-[1px] md:border-l-[1px] md:border-t-[0px] flex flex-col items-start justify-center border-slate-300 ">
+
+                    <img src={IMAGENES[0]} alt="" />
+
+                    <h5 className="flex capitalize items-center text-center mx-auto w-11/12 p-2">
+                      Datos del Arriendo
+                    </h5>
+
+                    <h3 className="flex capitalize gap-4 items-center mx-auto w-11/12 p-2  ">
+                      <IoBedOutline/>
+                      <span>
+                        {NUMERO_HABITACION > 1 ? `${NUMERO_HABITACION} Habitaciones` : `${NUMERO_HABITACION} habitación`}
+                      </span>
+                    </h3>
+
+                    <h3 className="flex capitalize gap-4 items-center mx-auto w-11/12 p-2 ">
+                      <TbBath />
+                        <span>
+                          {NUMERO_BANNO > 1 ? `${NUMERO_BANNO} Baños` : `${NUMERO_BANNO} Baño`}
+                        </span>
+                    </h3>
+
+                    <h3 className="flex capitalize gap-4 items-center mx-auto w-11/12 p-2">
+                      <span>
+                        <BsBuilding />
+                      </span>
+                        {NOMBRE}
+                    </h3>
+                    
+                    <h3 className="flex capitalize gap-4 items-center mx-auto w-11/12 p-2">
+                      <MdOutlineChair/>
+                        <span>
+                          {DESCRIPCION}
+                        </span>
+                    </h3>
+
+                    <h3 className="flex lowercase  gap-2 items-center mx-auto w-11/12 p-2">
+                      <span>
+                        <BiMap />
+                      </span>
+                        {DIRECCION}, {UBICACION}
+                    </h3>
+
+                    <h3 className=" flex-col capitalize gap-4 items-center mx-auto w-11/12 p-2">
+                      Precio total:  {newValorArriendo}
+                    </h3>
+                 
+                    <h6 className="flex-col capitalize gap-4 items-center mx-auto w-11/12 p-2">
+                      Abono del Arriendo:
+                    </h6>
+              </div>
             </div>
             <button className="Butto bg-purple-600 flex h-auto w-11/12 md:w-1/4 mx-auto mt-4 px-4 py-2 justify-center  hover:bg-purple-700 font-semibold hover:text-white uppercase rounded-2xl" onClick={null}>Proceder a Pagar</button>
           </div>
