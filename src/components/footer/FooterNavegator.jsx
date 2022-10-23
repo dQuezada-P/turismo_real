c4a547a16fd0120f8a83566badc9896b3157e30c
Diego Quezada <71034913+dQuezada-P@users.noreply.github.com>
2022-10-23 12:14:40 -0300

    login udate

--- src/components/footer/FooterNavegator.jsx
+++ src/components/footer/FooterNavegator.jsx
@@ -3,7 +3,7 @@ import FooterSection from "../footer/FooterSection";
 export default function FooterNavegator() {
   return (
     <nav className="md:flex md:gap-20 items-center md:mr-12 mt-14 md:mt-0 mx-auto md:mx-0 ml-9 md:ml-0">
-      <h2 className="font-semibold md:text-2xl text-sm text-white">Turismo Real 2012 - 2022 ©. Todos los derechos reservados.</h2>
+      <h2 className="font-semibold md:text-lg text-sm text-white">Turismo Real 2012 - 2022 ©. Todos los derechos reservados.</h2>
     </nav>
   );
 }
