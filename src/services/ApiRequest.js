1b6fe34d8a30c853a8934d68c37d81d609b08df2
lampiosko <54522763+lampiosko@users.noreply.github.com>
2022-11-26 17:43:20 -0300

    s

--- src/services/ApiRequest.js
+++ src/services/ApiRequest.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const HttpGet = (endpoint, token) => {
     try {
-        return axios.get('http://localhost:3000/api' + endpoint, { headers: { authorization: token } })
+        return axios.get('http://52.91.212.222:3000/api' + endpoint, { headers: { authorization: token } })
         .then(response =>{ return response.data})
     } catch (error) {
         console.log(error)
@@ -11,7 +11,7 @@ export const HttpGet = (endpoint, token) => {
 
 export const HttpPost = (endpoint, data, token = null) => {
     try {
-        return axios.post('http://localhost:3000/api' + endpoint, data, { headers: { authorization: token } })
+        return axios.post('http://52.91.212.222:3000/api' + endpoint, data, { headers: { authorization: token } })
         .then(response =>{ return response.data})
     } catch (error) {
         console.log(error)
@@ -20,7 +20,7 @@ export const HttpPost = (endpoint, data, token = null) => {
 
 export const HttpPut = (endpoint, data, token = null) => {
     try {
-        return axios.put('http://localhost:3000/api' + endpoint, data, { headers: { authorization: token } })
+        return axios.put('http://52.91.212.222/api' + endpoint, data, { headers: { authorization: token } })
         .then(response =>{ return response.data })
     } catch (error) {
         console.log(error)
